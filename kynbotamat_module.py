d9b45ebe165488a95b0afa47650bc0f023d33c16
tordisrml <tordis@rml.is>
2021-12-14 19:35:06 +0000

    14.12.2021

--- kynbotamat_module.py
+++ kynbotamat_module.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 import datetime
 import os
+import sys
+import datetime
+import time
+import math
 import shutil
 from matplotlib import pyplot as plt
 import seaborn as sns
 import statsmodels.formula.api as sm
+
+radnrkodifile = '../dmu_data/radnrkodi' #Created by prep_tdm.f
 
 #---------------------------------------------------------------------------
 #Function to read tab/space sperated files with files
@@ -106,6 +112,7 @@ def countingoff (df0,trait,traitcount, df1):
     df = df[['sire', traitcount]]
     df = df.drop_duplicates(subset=['sire'])
     df.columns = ['id', traitcount]
+    df = df.loc[(df['id'] > 0)]
     df1 = pd.merge(left=df1, right=df[['id',traitcount]], on='id', how='outer').fillna(0, downcast='infer')
     return df1
 
@@ -115,7 +122,7 @@ yticks = [75,80,85,90,95,100,105,110]
 def plottingmean(axes,df,trait,ylabel,name):
     axes.set_xticks(xticks)
     axes.set_xticklabels(xticks, rotation=90)
-    axes.set_yticks(yticks)
+#    axes.set_yticks(yticks)
     axes.plot(df.groupby('BY')[trait].mean(), label=name, marker='.')
     axes.legend()
     axes.set_ylabel(ylabel)
@@ -126,7 +133,7 @@ def plottingmeansns(axes,df,x,y,ylabel,name):
     sns.regplot(x=x, y=y, data=df, ax=axes, label=name)
     axes.set_xticks(xticks)
     axes.set_xticklabels(xticks, rotation=90)
-    axes.set_yticks(yticks)
+#    axes.set_yticks(yticks)
     axes.legend()
     axes.set_ylabel(ylabel)
     axes.grid(axis='y')
@@ -137,3 +144,184 @@ def plottingmeansns(axes,df,x,y,ylabel,name):
     # axes.set_title(f'{(params.loc[params.index == y].astype(str))},rsquared = {result.rsquared:.2f} ', fontsize=8)
     axes.annotate(f"{params.loc[params.index == y]}",xy = (2006,82))
     axes.annotate(f"rsquared = {result.rsquared:.2f}",xy = (2006,76))
+
+
+def yieldcollect(filename,t1,t2,t3,ID, per1, per2, per3):
+    file=filename
+    LP0, LP1, LP2, LP3, Wil=[], [], [], [], []
+    # Note that the data only includes milk records from dim=5 to dim=305.
+    # Therefore, (I think) I should only use these 301 days (rather than 305)
+    # Note that these computations are according to appendix G in Mrode.
+    for i in range(1,301):
+        stdim = -1+2*(i-1)/(305-5)
+        LP0.append(1)
+        LP1.append(1.2247*stdim)       #L2 in solsaman
+        LP2.append(-0.7906 + 2.3717*stdim**2)   #L3 in solsaman
+        LP3.append(-2.8067*stdim + 4.6771*stdim**3)   #L4 in solsaman
+        Wil.append(math.exp(-0.05*(i+5)))
+    Lyield = np.array([LP0,LP1,Wil])
+    Lper1 = Lyield[:,45]
+    Lper2 = Lyield[:,46:246]
+    Lyield = Lyield[:,5:296]
+    Lscs = np.array([LP0,LP1,LP2])
+    Lscs = Lscs[:,5:296]
+    Lfppp = np.array([LP0,LP1,LP2,LP3])
+    Lfppp = Lfppp[:,5:296]
+
+    a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4,ShortID = [],[],[],[],[],[],[],[],[],[],[],[],[]
+    d1,d2,d3,e1,e2,e3,f1,f2,f3, ShortIDPE = [],[],[],[],[],[],[],[],[],[]
+
+    if ((t1 == 'my1')
+        | (t1 == 'fy1')
+        | (t1 == 'py1')
+        | (t1 == 'scs1')
+        ):
+        print(f'Reading {filename}')
+        with open(file, 'r') as fp:
+             for cnt, line in enumerate(fp):
+                 if line[0]=='4':
+                    i = line.split()
+                    if i[2]=='1':
+                        ShortID.append(i[4])
+                        a1.append(float(i[7]))
+                    elif i[2]=='2':
+                        a2.append(float(i[7]))
+                    elif i[2]=='3':
+                        a3.append(float(i[7]))
+                    elif i[2]=='4':
+                        b1.append(float(i[7]))
+                    elif i[2]=='5':
+                        b2.append(float(i[7]))
+                    elif i[2]=='6':
+                        b3.append(float(i[7]))
+                    elif i[2]=='7':
+                        c1.append(float(i[7]))
+                    elif i[2]=='8':
+                        c2.append(float(i[7]))
+                    elif i[2]=='9':
+                        c3.append(float(i[7]))
+                 # elif line[0]=='3' and line.split()[3]=='3':
+                 #    i = line.split()
+                 #    if i[2]=='1':
+                 #        ShortIDPE.append(i[4])
+                 #        d1.append(float(i[7]))
+                 #    elif i[2]=='2':
+                 #        d2.append(float(i[7]))
+                 #    elif i[2]=='3':
+                 #        d3.append(float(i[7]))
+                 #    elif i[2]=='4':
+                 #        e1.append(float(i[7]))
+                 #    elif i[2]=='5':
+                 #        e2.append(float(i[7]))
+                 #    elif i[2]=='6':
+                 #        e3.append(float(i[7]))
+                 #    elif i[2]=='7':
+                 #        f1.append(float(i[7]))
+                 #    elif i[2]=='8':
+                 #        f2.append(float(i[7]))
+                 #    elif i[2]=='9':
+                 #        f3.append(float(i[7]))
+
+        aList = np.array([a1, a2, a3],dtype='float')
+        bList = np.array([b1, b2, b3],dtype='float')
+        cList = np.array([c1, c2, c3],dtype='float')
+        # dList = np.array([d1, d2, d3],dtype='float')
+        # eList = np.array([e1, e2, e3],dtype='float')
+        # fList = np.array([f1, f2, f3],dtype='float')
+
+        print(file, 'read.')
+        print('Solutions written to numpy arrays a, b and c.')
+
+        if ((t1 == 'my1')
+            | (t1 == 'fy1')
+            | (t1 == 'py1')
+            ):
+
+            #Multiply matrices to obtain solutions.
+            vec1 = np.dot(aList.transpose(), Lyield)
+            vec2 = np.dot(bList.transpose(), Lyield)
+            vec3 = np.dot(cList.transpose(), Lyield)
+            # vec4 = np.dot(dList.transpose(), Lyield)
+            # vec5 = np.dot(eList.transpose(), Lyield)
+            # vec6 = np.dot(fList.transpose(), Lyield)
+
+            #Multiply matrices to obtain solutions.
+            vec1per = (-(np.dot(aList.transpose(), Lper1))*200) + (np.dot(aList.transpose(), Lper2)).sum(1)
+            vec2per = (-(np.dot(bList.transpose(), Lper1))*200) + (np.dot(bList.transpose(), Lper2)).sum(1)
+            vec3per = (-(np.dot(cList.transpose(), Lper1))*200) + (np.dot(cList.transpose(), Lper2)).sum(1)
+
+            df = list(zip(ShortID, vec1.sum(1), vec2.sum(1), vec3.sum(1), vec1per, vec2per, vec3per))
+            df = sorted(df, key = lambda id: id[0])
+
+            df = pd.DataFrame(df, columns = [ID, t1, t2, t3, per1, per2, per3])
+            df[ID] = df[ID].astype(int)
+
+        if (t1 == 'scs1'):
+
+            #Multiply matrices to obtain solutions.
+            vec1 = np.dot(aList.transpose(), Lscs)
+            vec2 = np.dot(bList.transpose(), Lscs)
+            vec3 = np.dot(cList.transpose(), Lscs)
+            # vec4 = np.dot(dList.transpose(), Lscs)
+            # vec5 = np.dot(eList.transpose(), Lscs)
+            # vec6 = np.dot(fList.transpose(), Lscs)
+
+            df = list(zip(ShortID, vec1.sum(1), vec2.sum(1), vec3.sum(1)))
+            df = sorted(df, key = lambda id: id[0])
+
+            df = pd.DataFrame(df, columns = [ID, t1, t2, t3])
+            df[ID] = df[ID].astype(int)
+
+    if ((t1 == 'pp1')
+        | (t1 == 'fp1')
+        ):
+        print(f'Reading {filename}')
+        with open(file, 'r') as fp:
+             for cnt, line in enumerate(fp):
+                 if line[0]=='4':
+                    i = line.split()
+                    if i[2]=='1':
+                        ShortID.append(i[4])
+                        a1.append(float(i[7]))
+                    elif i[2]=='2':
+                        a2.append(float(i[7]))
+                    elif i[2]=='3':
+                        a3.append(float(i[7]))
+                    elif i[2]=='4':
+                        a4.append(float(i[7]))
+                    elif i[2]=='5':
+                        b1.append(float(i[7]))
+                    elif i[2]=='6':
+                        b2.append(float(i[7]))
+                    elif i[2]=='7':
+                        b3.append(float(i[7]))
+                    elif i[2]=='8':
+                        b4.append(float(i[7]))
+                    elif i[2]=='9':
+                        c1.append(float(i[7]))
+                    elif i[2]=='10':
+                        c2.append(float(i[7]))
+                    elif i[2]=='11':
+                        c3.append(float(i[7]))
+                    elif i[2]=='12':
+                        c4.append(float(i[7]))
+
+        aList = np.array([a1, a2, a3, a4],dtype='float')
+        bList = np.array([b1, b2, b3, b4],dtype='float')
+        cList = np.array([c1, c2, c3, c4],dtype='float')
+
+        print(file, 'read.')
+        print('Solutions written to numpy arrays a, b and c.')
+
+        #Multiply matrices to obtain solutions.
+        vec1 = np.dot(aList.transpose(), Lfppp)
+        vec2 = np.dot(bList.transpose(), Lfppp)
+        vec3 = np.dot(cList.transpose(), Lfppp)
+
+        df = list(zip(ShortID, vec1.sum(1), vec2.sum(1), vec3.sum(1)))
+        df = sorted(df, key = lambda id: id[0])
+
+        df = pd.DataFrame(df, columns = [ID, t1, t2, t3])
+        df[ID] = df[ID].astype(int)
+
+    return df
