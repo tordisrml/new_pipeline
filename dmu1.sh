760cf332b7735efe065ae32fbba89c2c390921cd
tordisrml <tordis@rml.is>
2022-01-13 11:27:36 +0000

    13.01.2022

---
+++ dmu1.sh
@@ -0,0 +1,3 @@
+#/home/LBHI/elsa/DMUV6/dmuv6/R4.7/bin/dmu1<dir>dmu1 &
+#/home/elsa/dmuv6/dmuv6/R5.1/bin/dmu1<dir>dmu1 &
+/home/thordis/DMU/dmuv6/R5.3-EM64T/bin/dmu1<dir>dmu1 &
---
+++ dmu5.sh
@@ -0,0 +1,3 @@
+ulimit -s unlimited
+#/home/elsa/dmuv6/dmuv6/R5.1/bin/dmu5>dmu5 &
+/home/thordis/DMU/dmuv6/R5.3-EM64T/bin/dmu5>dmu5 &
